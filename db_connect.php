afd54b163aed572c2aa53f48e816e654cfdee027
Cory Wattenberger <CWattenber@my.gcu.edu>
2023-08-22 19:57:13 -0400

    .

--- db_connect.php
+++ db_connect.php
@@ -1,14 +1,22 @@
 <?php
-// Attempt to establish a connection with the SQL Server using PDO
+// Attempt to establish a connection with the MySQL database using PDO
 try {
-    // Create a new PDO instance for SQL Server with the provided credentials and settings
-    $conn = new PDO("sqlsrv:server = tcp:cst323a3server.database.windows.net,1433; Database = BlogsDB-Act3", "snywdnrydv", "J60123G3V3J4ZN2B$");
+    // Credentials and settings
+    $host = 'xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    $db = 'y34bhol13013t4m4';
+    $user = 'u3nm9urxv14doy8a';
+    $pass = 'equth4sc1f7vku8j';
+    $charset = 'utf8';
 
-    // Set the PDO error mode attribute to throw exceptionsfor error handling
+    // Create a new PDO instance for MySQL with the provided credentials and settings
+    $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+    $conn = new PDO($dsn, $user, $pass);
+
+    // Set the PDO error mode attribute to throw exceptions for error handling
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (PDOException $e) {
     // If there's an issue connecting to the database, terminate the script and display an error message
-    die("Error connecting to SQL Server: " . $e->getMessage());
+    die("Error connecting to MySQL: " . $e->getMessage());
 }
 ?>
